75120cbbd8883c42ec329d27ce505b1f52c64954
Kowsar Mahmud Pappu <kowsarmahmud156@gmail.com>
2024-04-25 22:59:36 +0600

    Third Test Failed

--- src/test/java/org/example/MovieTest.java
+++ src/test/java/org/example/MovieTest.java
@@ -22,5 +22,20 @@ class MovieTest {
         app.addMovie(movie);
         assertEquals(1, app.getMovies().size());
     }
+
+    @Test
+    void testGetMovieDetails() {
+        MovieListingApp app = new MovieListingApp();
+        Movie movie = new Movie("Dune: Part Two", Arrays.asList("Timothée Chalamet", "Zendaya", "Rebecca Ferguson", "Florence Pugh", "Austin Butler"), "Science Fiction", "2024", 190000000);
+        app.addMovie(movie);
+
+        Movie retrievedMovie = app.getMovieDetails("Dune: Part Two");
+        assertNotNull(retrievedMovie);
+        assertEquals("Dune: Part Two", retrievedMovie.getTitle());
+        assertEquals(Arrays.asList("Timothée Chalamet", "Zendaya", "Rebecca Ferguson", "Florence Pugh", "Austin Butler"), retrievedMovie.getCast());
+        assertEquals("Science Fiction", retrievedMovie.getCategory());
+        assertEquals("2024", retrievedMovie.getReleaseDate());
+        assertEquals(190000000, retrievedMovie.getBudget());
+    }
 }
 
